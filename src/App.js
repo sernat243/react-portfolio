db5e731e020ffced8af4216617ff0a597795a9c4
Gabriel <sernat243@gmail.com>
2023-10-26 16:11:38 -0600

    created aboutme component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from "./components/Header";
 import Projects from "./components/Projects";
+import AboutMe from './components/AboutMe';
 
 function App() {
   return (
     <div>
       <Header />
       <Projects />
+      <AboutMe />
     </div>
   );
 }
---
+++ src/components/AboutMe.js
@@ -0,0 +1,23 @@
+const AboutMe = () => {
+    return (
+      <section>
+        <h2>About Me</h2>
+        <p>
+          I am a passionate freelance developer with a strong interest in web
+          development. I love creating responsive and user-friendly web
+          applications using the latest technologies.
+        </p>
+        <p>
+          My goal is to provide valuable solutions and build awesome projects. I
+          am constantly learning and exploring new technologies to stay
+          up-to-date in this ever-evolving field.
+        </p>
+        <p>
+          When I'm not coding, you can find me exploring the outdoors, reading
+          books, or working on personal coding projects.
+        </p>
+      </section>
+    );
+  };
+  
+  export default AboutMe;
