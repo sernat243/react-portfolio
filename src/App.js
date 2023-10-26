f7994ca3b58a8889934d0dd42ca423827dab750f
Gabriel <sernat243@gmail.com>
2023-10-26 14:50:19 -0600

    created simple projects component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from "./components/Header";
+import Projects from "./components/Projects";
 
 function App() {
   return (
     <div>
       <Header />
+      <Projects />
     </div>
   );
 }
---
+++ src/components/Projects.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+
+const Projects = () => {
+    const [repos, setRepos] = useState([]);
+
+    useEffect(() => {
+        const reposUrl = 'https://api.github.com/users/sernat243/repos';
+
+        fetch(reposUrl)
+            .then((response) => response.json())
+            .then((data) => setRepos(data))
+            .catch((error) => console.error("Error fetching GitHub data: ", error));
+    }, []);
+
+    return (
+        <section>
+            <h2>Projects</h2>
+            <ul>
+                {repos.map((repo) => (
+                    <li key={repo.id}>
+                        <a href={repo.html_url} target="_blank" rel="noopener noreferrer">
+                            {repo.name}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+        </section>
+    );
+};
+
+export default Projects;
