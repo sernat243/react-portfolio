5e9463a9a9110a12299f3ff096a688114de9b0d1
Gabriel <sernat243@gmail.com>
2023-10-26 14:25:11 -0600

    created simple header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Gabriel Serna</h1>
+            <p>Freelance Developer</p>
+        </header>
+    );
+};
+
+export default Header;
