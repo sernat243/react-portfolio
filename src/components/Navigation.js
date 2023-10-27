d3f975422fa4be311ffbeb2ee960c66204379f24
Gabriel <sernat243@gmail.com>
2023-10-27 12:01:56 -0600

    removed activeclass from nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,16 +5,16 @@ const Navigation = () => {
     <nav>
       <ul className="list-unstyled d-flex justify-content-end">
         <li className="me-3">
-          <NavLink to="/about" activeClassName="selected-link" className="text-light text-decoration-none">About Me</NavLink>
+          <NavLink to="/about" className="text-light text-decoration-none">About Me</NavLink>
         </li>
         <li className="me-3">
-          <NavLink to="/projects" activeClassName="selected-link" className="text-light text-decoration-none">Projects</NavLink>
+          <NavLink to="/projects" className="text-light text-decoration-none">Projects</NavLink>
         </li>
         <li className="me-3">
-          <NavLink to="/contact" activeClassName="selected-link" className="text-light text-decoration-none">Contact</NavLink>
+          <NavLink to="/contact" className="text-light text-decoration-none">Contact</NavLink>
         </li>
         <li>
-          <NavLink to="/resume" activeClassName="selected-link" className="text-light text-decoration-none">Resume</NavLink>
+          <NavLink to="/resume" className="text-light text-decoration-none">Resume</NavLink>
         </li>
       </ul>
     </nav>
