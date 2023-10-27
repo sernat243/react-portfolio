b0f1d4575db47d286a94b8c95b53c5a741e5188d
Gabriel <sernat243@gmail.com>
2023-10-26 23:01:10 -0600

    added error message to message input field

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,6 +8,8 @@ const Contact = () => {
         message: '',
     });
 
+    const [messageError, setMessageError] =useState('');
+
     const handleInputChange = (e) => {
         const { name, value } = e.target;
         setFormData({
@@ -27,41 +29,52 @@ const Contact = () => {
         });
     };
 
+    const handleBlur = () => {
+        if (formData.message.trim() === '') {
+          setMessageError('Message is required');
+        } else {
+          setMessageError('');
+        }
+      };
+    
+
     return (
       <section id="contact">
         <h2>Contact Me</h2>
         <form onSubmit={handleSubmit}>
             <div className="form-group">
-            <label htmlFor="name">Name</label>
-            <input
-                type="text"
-                id="name"
-                name="name"
-                value={formData.name}
-                onChange={handleInputChange}
-                required
-            />
+                <label htmlFor="name">Name</label>
+                <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    value={formData.name}
+                    onChange={handleInputChange}
+                    required
+                />
             </div>
             <div className="form-group">
-            <label htmlFor="email">Email Address</label>
-            <input
-                type="email"
-                id="email"
-                name="email"
-                value={formData.email}
-                onChange={handleInputChange}
-                required
-            />
+                <label htmlFor="email">Email Address</label>
+                <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleInputChange}
+                    required
+                />
             </div>
             <div className="form-group">
-            <label htmlFor="message">Message</label>
-            <textarea
-                id="message"
-                name="message"
-                value={formData.message}
-                onChange={handleInputChange}
-                required
-            />
+                <label htmlFor="message">Message</label>
+                <textarea
+                    id="message"
+                    name="message"
+                    value={formData.message}
+                    onChange={handleInputChange}
+                    onBlur={handleBlur}
+                    required
+                />
+                {messageError && <p className="error-message">{messageError}</p>}
             </div>
             <button type="submit">Submit</button>
         </form>
