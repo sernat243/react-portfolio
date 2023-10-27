101d32354d6e2984d457785f89073491c31f4481
Gabriel <sernat243@gmail.com>
2023-10-26 20:23:25 -0600

    made about the default route

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,17 @@
-import { Route, Routes as AppRoutes } from "react-router-dom";
+import { Route, Routes as AppRoutes, Navigate } from "react-router-dom";
 import AboutMe from "./components/AboutMe";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Resume from "./components/Resume";
 
+const DefaultRoute = () => {
+  return <Navigate to="/about" />;
+}
+
 const Routes = () => {
     return (
       <AppRoutes>
+        <Route path="/" element={<DefaultRoute />} />
         <Route path="/about" element={<AboutMe />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
